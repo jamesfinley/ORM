058141e3a60edfab56b2722010cc0f2c63608adb
James Finley <jamesfinley@gmail.com>
2013-08-23 14:14:51 -0500

    Added joins, having, and group by to DB class. Added having to Model

--- db.php
+++ db.php
@@ -5,10 +5,13 @@ class DB {
 	public $db = null;
 	private $queries = array();
 	private $where = array();
+	private $having = array();
 	private $select = '*';
+	private $join = array();
 	private $order = null;
 	private $limit = null;
 	private $offset = null;
+	private $group = null;
 	
 	public static function Instance($host = null, $user = null, $pass = null, $database = null)
 	{
@@ -24,16 +27,22 @@ class DB {
 	{
 		$db = new mysqli($host, $user, $pass, $database);
 		$this->db = $db;
+		$this->queries = array();
 		$this->where = array();
+		$this->having = array();
+		$this->join = array();
 	}
 	
 	private function reset()
 	{
 		$this->where = array();
+		$this->having = array();
 		$this->select = '*';
+		$this->join = array();
 		$this->order = null;
 		$this->limit = null;
 		$this->offset = null;
+		$this->group = null;
 	}
 	
 	//$query can be either a field name or an associative array of key/value pairs
@@ -84,11 +93,85 @@ class DB {
 		return '';
 	}
 	
+	//$query can be either a field name or an associative array of key/value pairs
+	public function having($query, $value = null)
+	{
+		if (!is_array($query) && $value)
+		{
+			$query = array(
+				$query => $value
+			);
+		}
+		if (is_array($query))
+		{
+			foreach ($query as $key => $value)
+			{
+				$this->having[] = array($key, $value);
+			}
+		}
+		else
+		{
+			$this->having[] = array($query);
+		}
+		
+		return $this;
+	}
+	
+	private function build_having()
+	{
+		if (count($this->having) > 0)
+		{
+			$query = ' HAVING ';
+			$sets = array();
+			foreach ($this->having as $where)
+			{
+				if (count($where) == 2)
+				{
+					$sets[] = $where[0] . ' = ' . $where[1];
+				}
+				else
+				{
+					$sets[] = $where[0];
+				}
+			}
+			$query .= implode(' AND ', $sets);
+			
+			return $query;
+		}
+		return '';
+	}
+	
 	public function select($select)
 	{
 		$this->select = $select;
 		
 		return $this;
+	}
+	
+	public function join($table_name, $on, $type = '')
+	{
+		$this->join[] = array(
+			'table_name' 	=> $table_name,
+			'on' 			=> $on,
+			'type' 			=> $type
+		);
+		
+		return $this;
+	}
+	
+	private function build_join()
+	{
+		if (count($this->join))
+		{
+			$joins = [];
+			foreach ($this->join as $j)
+			{
+				$join = ($j['type'] != '' ? $j['type'] . ' ' : '') . $j['table_name'] . ' ON ' . $j['on'];
+				$joins[] = $join;
+			}
+			return ' '.implode(' ', $joins);
+		}
+		return '';
 	}
 	
 	public function order($field, $order = 'ASC')
@@ -103,6 +186,22 @@ class DB {
 		if ($this->order !== null)
 		{
 			return ' ORDER BY ' . $this->order;
+		}
+		return '';
+	}
+	
+	public function group($field)
+	{
+		$this->group = $field;
+		
+		return $this;
+	}
+	
+	private function build_group()
+	{
+		if ($this->group !== null)
+		{
+			return ' GROUP BY ' . $this->group;
 		}
 		return '';
 	}
@@ -138,7 +237,7 @@ class DB {
 			$this->where($where);
 		}
 		
-		$query = 'SELECT ' . $this->select . ' FROM ' . $table_name . $this->build_where() . $this->build_order() . $this->build_limit();
+		$query = 'SELECT ' . $this->select . ' FROM ' . $table_name . $this->build_join() . $this->build_where() . $this->build_group() . $this->build_having() . $this->build_order() . $this->build_limit();
 		
 		$this->reset();
 		
